a10bb47319e31bf77a35d0d879cdd98b5cd80d01
Joe Carder <jcarder@perforce.com>
2025-09-09 17:18:52 -0600

    Update outputs for correct trail

--- outputs.tf
+++ outputs.tf
@@ -12,10 +12,10 @@ output "private_zone_ids" {
 
 output "api_nlb_dns_name" {
   description = "API/MCS NLB DNS names per cluster (from module)"
-  value       = { for k, m in module.ocp_compact : k => m.nlb_dns }
+  value       = { for k, m in module.cluster : k => m.nlb_dns }
 }
 
 output "private_zone_id" {
   description = "Private hosted zone IDs per cluster (from module)"
-  value       = { for k, m in module.ocp_compact : k => m.private_zone_id }
+  value       = { for k, m in module.cluster : k => m.private_zone_id }
 }
