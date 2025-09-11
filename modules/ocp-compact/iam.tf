649830aece61531b1020233c55bdf788610dce9a
Joe Carder <jcarder@perforce.com>
2025-09-11 11:04:37 -0600

    add LB actions to role

--- modules/ocp-compact/iam.tf
+++ modules/ocp-compact/iam.tf
@@ -1,5 +1,4 @@
-# modules/ocp-compact/iam.tf
-
+# EC2 trust so instances can assume the role
 data "aws_iam_policy_document" "ec2_trust" {
   statement {
     effect = "Allow"
@@ -11,6 +10,7 @@ data "aws_iam_policy_document" "ec2_trust" {
   }
 }
 
+# Read-only EC2 describes used widely (incl. CCM)
 data "aws_iam_policy_document" "ccm_readonly" {
   statement {
     effect = "Allow"
@@ -24,10 +24,73 @@ data "aws_iam_policy_document" "ccm_readonly" {
       "ec2:DescribeSubnets",
       "ec2:DescribeSecurityGroups",
       "ec2:DescribeRouteTables",
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeInternetGateways",
     ]
     resources = ["*"]
   }
 }
+
+# ELBv2 + supporting EC2/IAM calls needed for Service type=LoadBalancer (router)
+data "aws_iam_policy_document" "ccm_elbv2" {
+  # ELBv2 lifecycle + describe
+  statement {
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:CreateLoadBalancer",
+      "elasticloadbalancing:DeleteLoadBalancer",
+      "elasticloadbalancing:CreateTargetGroup",
+      "elasticloadbalancing:DeleteTargetGroup",
+      "elasticloadbalancing:RegisterTargets",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:CreateListener",
+      "elasticloadbalancing:DeleteListener",
+      "elasticloadbalancing:ModifyLoadBalancerAttributes",
+      "elasticloadbalancing:ModifyTargetGroup",
+      "elasticloadbalancing:ModifyTargetGroupAttributes",
+      "elasticloadbalancing:SetIpAddressType",
+      "elasticloadbalancing:SetSubnets",
+      "elasticloadbalancing:AddTags",
+      "elasticloadbalancing:RemoveTags",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeTargetHealth",
+      "elasticloadbalancing:DescribeListeners",
+      "elasticloadbalancing:DescribeLoadBalancerAttributes",
+      "elasticloadbalancing:DescribeTargetGroupAttributes",
+      "elasticloadbalancing:DescribeTags",
+    ]
+    resources = ["*"]
+  }
+
+  # Service-linked role creation (first time per account/region)
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:CreateServiceLinkedRole"]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "iam:AWSServiceName"
+      values   = ["elasticloadbalancing.amazonaws.com"]
+    }
+  }
+
+  # Supporting EC2 calls the controller uses while wiring the LB
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:CreateTags",
+      "ec2:DeleteTags",
+      "ec2:CreateSecurityGroup",
+      "ec2:DeleteSecurityGroup",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupIngress",
+    ]
+    resources = ["*"]
+  }
+}
+
+# ----- Role & instance profile for masters -----
 
 resource "aws_iam_role" "master" {
   name               = "${var.cluster}-master-role"
@@ -41,8 +104,15 @@ resource "aws_iam_instance_profile" "master" {
   tags = merge(local.tags_base, { Resource = "iam-instance-profile-master" })
 }
 
+# Attach policies to the master role
 resource "aws_iam_role_policy" "master_readonly" {
   name   = "${var.cluster}-master-ccm-readonly"
   role   = aws_iam_role.master.id
   policy = data.aws_iam_policy_document.ccm_readonly.json
+}
+
+resource "aws_iam_role_policy" "master_elbv2" {
+  name   = "${var.cluster}-master-elbv2"
+  role   = aws_iam_role.master.id
+  policy = data.aws_iam_policy_document.ccm_elbv2.json
 }
