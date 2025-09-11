9908041fa57031fac813fe619641b6e983ef1b6b
Joe Carder <jcarder@perforce.com>
2025-09-11 10:50:04 -0600

    Added missing actions for routetables list role policies

--- modules/ocp-compact/iam.tf
+++ modules/ocp-compact/iam.tf
@@ -23,6 +23,7 @@ data "aws_iam_policy_document" "ccm_readonly" {
       "ec2:DescribeVpcs",
       "ec2:DescribeSubnets",
       "ec2:DescribeSecurityGroups",
+      "ec2:DescribeRouteTables",
     ]
     resources = ["*"]
   }
