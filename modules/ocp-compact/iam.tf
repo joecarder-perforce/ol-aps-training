cf2268175858a6b46a394aeaf00d0bf1a6bc5a29
Joe Carder <jcarder@perforce.com>
2025-09-11 12:16:37 -0600

    expanding role permssions considerably to fix one at time fixes

--- modules/ocp-compact/iam.tf
+++ modules/ocp-compact/iam.tf
@@ -1,77 +1,119 @@
-# EC2 trust so instances can assume the role
+# --- Trust policy for EC2 instances (unchanged) ---
 data "aws_iam_policy_document" "ec2_trust" {
   statement {
     effect = "Allow"
+
     principals {
       type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
     }
+
     actions = ["sts:AssumeRole"]
   }
 }
 
-# Read-only EC2 describes used widely (incl. CCM)
+# --- Read/Describe + basic EC2 edits used by the KCM service-controller ---
 data "aws_iam_policy_document" "ccm_readonly" {
   statement {
     effect = "Allow"
     actions = [
-      "ec2:DescribeInstances",
-      "ec2:DescribeRegions",
-      "ec2:DescribeAvailabilityZones",
-      "ec2:DescribeTags",
-      "ec2:DescribeImages",
-      "ec2:DescribeVpcs",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeRouteTables",
+      # EC2 discovery
       "ec2:DescribeAccountAttributes",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeImages",
+      "ec2:DescribeInstances",
       "ec2:DescribeInternetGateways",
+      "ec2:DescribeRegions",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeTags",
+      "ec2:DescribeVpcs",
+
+      # SG + Tag management sometimes used with LB wiring
+      "ec2:CreateSecurityGroup",
+      "ec2:DeleteSecurityGroup",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupIngress",
+      "ec2:CreateTags",
+      "ec2:DeleteTags",
     ]
     resources = ["*"]
   }
 }
 
-# ELBv2 + supporting EC2/IAM calls needed for Service type=LoadBalancer (router)
-data "aws_iam_policy_document" "ccm_elbv2" {
-  # ELBv2 lifecycle + describe
+# --- Full ELB (classic) + ELBv2 set for Service type=LoadBalancer from Ingress ---
+data "aws_iam_policy_document" "master_elb" {
   statement {
+    sid    = "ClassicELB"
     effect = "Allow"
     actions = [
+      # Classic ELB lifecycle
       "elasticloadbalancing:CreateLoadBalancer",
       "elasticloadbalancing:DeleteLoadBalancer",
-      "elasticloadbalancing:CreateTargetGroup",
-      "elasticloadbalancing:DeleteTargetGroup",
-      "elasticloadbalancing:RegisterTargets",
-      "elasticloadbalancing:DeregisterTargets",
-      "elasticloadbalancing:CreateListener",
-      "elasticloadbalancing:CreateLoadBalancerPolicy",
-      "elasticloadbalancing:DeleteListener",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DescribeLoadBalancerAttributes",
       "elasticloadbalancing:ModifyLoadBalancerAttributes",
-      "elasticloadbalancing:ModifyTargetGroup",
-      "elasticloadbalancing:ModifyTargetGroupAttributes",
-      "elasticloadbalancing:SetIpAddressType",
-      "elasticloadbalancing:SetSubnets",
+      "elasticloadbalancing:DescribeListeners",
       "elasticloadbalancing:AddTags",
       "elasticloadbalancing:RemoveTags",
-      "elasticloadbalancing:DescribeLoadBalancers",
-      "elasticloadbalancing:DescribeTargetGroups",
-      "elasticloadbalancing:DescribeTargetHealth",
-      "elasticloadbalancing:DescribeListeners",
-      "elasticloadbalancing:DescribeLoadBalancerAttributes",
-      "elasticloadbalancing:DescribeTargetGroupAttributes",
       "elasticloadbalancing:DescribeTags",
-      "elasticloadbalancing:DeleteLoadBalancerPolicy",
-      "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+
+      # Classic ELB health checks & registration
+      "elasticloadbalancing:ConfigureHealthCheck",
+      "elasticloadbalancing:DescribeInstanceHealth",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+
+      # Classic ELB listeners & backend policies (needed for proxy protocol, etc.)
       "elasticloadbalancing:CreateLoadBalancerListeners",
       "elasticloadbalancing:DeleteLoadBalancerListeners",
+      "elasticloadbalancing:CreateLoadBalancerPolicy",
+      "elasticloadbalancing:DeleteLoadBalancerPolicy",
+      "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
       "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
-      "elasticloadbalancing:ConfigureHealthCheck",
     ]
     resources = ["*"]
   }
 
-  # Service-linked role creation (first time per account/region)
   statement {
+    sid    = "ELBv2NLB"
+    effect = "Allow"
+    actions = [
+      # ELBv2 lifecycle
+      "elasticloadbalancing:CreateLoadBalancer",
+      "elasticloadbalancing:DeleteLoadBalancer",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DescribeLoadBalancerAttributes",
+      "elasticloadbalancing:ModifyLoadBalancerAttributes",
+      "elasticloadbalancing:SetIpAddressType",
+      "elasticloadbalancing:SetSubnets",
+      "elasticloadbalancing:AddTags",
+      "elasticloadbalancing:RemoveTags",
+      "elasticloadbalancing:DescribeTags",
+
+      # Listeners
+      "elasticloadbalancing:CreateListener",
+      "elasticloadbalancing:DeleteListener",
+      "elasticloadbalancing:DescribeListeners",
+
+      # Target groups
+      "elasticloadbalancing:CreateTargetGroup",
+      "elasticloadbalancing:DeleteTargetGroup",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeTargetGroupAttributes",
+      "elasticloadbalancing:ModifyTargetGroup",
+      "elasticloadbalancing:ModifyTargetGroupAttributes",
+      "elasticloadbalancing:RegisterTargets",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:DescribeTargetHealth",
+    ]
+    resources = ["*"]
+  }
+
+  # Service-linked role for ELB if it doesn't exist yet
+  statement {
+    sid       = "ELBServiceLinkedRole"
     effect    = "Allow"
     actions   = ["iam:CreateServiceLinkedRole"]
     resources = ["*"]
@@ -81,24 +123,9 @@ data "aws_iam_policy_document" "ccm_elbv2" {
       values   = ["elasticloadbalancing.amazonaws.com"]
     }
   }
-
-  # Supporting EC2 calls the controller uses while wiring the LB
-  statement {
-    effect = "Allow"
-    actions = [
-      "ec2:CreateTags",
-      "ec2:DeleteTags",
-      "ec2:CreateSecurityGroup",
-      "ec2:DeleteSecurityGroup",
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:RevokeSecurityGroupIngress",
-    ]
-    resources = ["*"]
-  }
 }
 
-# ----- Role & instance profile for masters -----
-
+# --- Role & Instance Profile (unchanged names) ---
 resource "aws_iam_role" "master" {
   name               = "${var.cluster}-master-role"
   assume_role_policy = data.aws_iam_policy_document.ec2_trust.json
@@ -111,15 +138,15 @@ resource "aws_iam_instance_profile" "master" {
   tags = merge(local.tags_base, { Resource = "iam-instance-profile-master" })
 }
 
-# Attach policies to the master role
+# --- Attach policies to the role ---
 resource "aws_iam_role_policy" "master_readonly" {
   name   = "${var.cluster}-master-ccm-readonly"
   role   = aws_iam_role.master.id
   policy = data.aws_iam_policy_document.ccm_readonly.json
 }
 
-resource "aws_iam_role_policy" "master_elbv2" {
-  name   = "${var.cluster}-master-elbv2"
+resource "aws_iam_role_policy" "master_elb" {
+  name   = "${var.cluster}-master-elb"
   role   = aws_iam_role.master.id
-  policy = data.aws_iam_policy_document.ccm_elbv2.json
+  policy = data.aws_iam_policy_document.master_elb.json
 }
