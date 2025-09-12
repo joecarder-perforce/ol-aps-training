021a0badfe0289eae3ca3a756214cd95a672020c
Joe Carder <jcarder@perforce.com>
2025-09-12 09:06:23 -0600

    tofu fmt fixes

--- dns-add.tf
+++ dns-add.tf
@@ -2,15 +2,15 @@
 resource "aws_route53_zone" "private" {
   for_each = local.active_clusters
 
-  name = "${each.key}.${var.base_domain}"   # e.g. s0.trng.lab
+  name = "${each.key}.${var.base_domain}" # e.g. s0.trng.lab
 
   # AWS requires at least one VPC association at create time
   vpc {
-    vpc_id = var.jump_vpc_id              
+    vpc_id = var.jump_vpc_id
   }
 
   comment = "Private zone for ${each.key}.${var.base_domain}"
-  tags    = merge(var.common_tags, try(each.value.extra_tags, {}), {
+  tags = merge(var.common_tags, try(each.value.extra_tags, {}), {
     Cluster  = each.key
     Resource = "route53-private-zone"
   })
