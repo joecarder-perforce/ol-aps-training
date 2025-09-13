9800f9b237ffe99e8ff469aef6e1ae1bdadc00af
Joe Carder <jcarder@perforce.com>
2025-09-13 06:33:32 -0600

    Fixed errors inside yaml spec causing a failure for spec files to be loaded on bootstrap correctly, add CCO credentials as passthrough

--- modules/ocp-compact/dns-manifest.tf
+++ modules/ocp-compact/dns-manifest.tf
@@ -1,56 +1,68 @@
 # Use the existing metadata_json_path to derive the installer WORKDIR
 locals {
-  workdir     = dirname(var.metadata_json_path)
-  apps_domain = "apps.${var.cluster}.${var.base_domain}"
+  workdir = dirname(var.metadata_json_path)
 }
 
-# Ensure the manifests directory exists
+# Ensure the manifests directory exists so openshift-install will pick them up
 resource "null_resource" "ensure_manifests_dir" {
-  triggers = {
-    path = "${local.workdir}/manifests"
-  }
-
+  triggers = { path = "${local.workdir}/manifests" }
   provisioner "local-exec" {
     command = "mkdir -p ${local.workdir}/manifests"
   }
 }
 
-# 1) DNS operator: point to your private Route53 zone (lets OCP manage records)
-resource "local_file" "dns_private_zone" {
-  filename = "${local.workdir}/manifests/zz-01-dns-private-zone.yaml"
-  content  = <<-YAML
-    apiVersion: operator.openshift.io/v1
-    kind: DNS
-    metadata:
-      name: default
-    spec:
-      privateZone:
-        id: ${var.private_zone_id}
-  YAML
+# --- CCO Passthrough so operators use node IAM to manage Route53 ---
+resource "local_file" "cco_passthrough" {
+  filename = "${local.workdir}/manifests/00-cco-passthrough.yaml"
+  content  = <<YAML
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cloud-credential-operator-config
+  namespace: openshift-cloud-credential-operator
+data:
+  mode: Passthrough
+YAML
 
   depends_on = [null_resource.ensure_manifests_dir]
 }
 
-# 2) Default IngressController: internal NLB and apps domain
+# --- DNS cluster CR: bind to existing PRIVATE hosted zone (short id, no /hostedzone/) ---
+resource "local_file" "dns_private_zone" {
+  filename = "${local.workdir}/manifests/zz-01-dns-cluster.yaml"
+  content  = <<YAML
+apiVersion: config.openshift.io/v1
+kind: DNS
+metadata:
+  name: cluster
+spec:
+  privateZone:
+    id: ${var.private_zone_id}
+  publicZone: null
+YAML
+
+  depends_on = [null_resource.ensure_manifests_dir]
+}
+
+# --- IngressController: internal NLB; keep default apps domain (no spec.domain) ---
 resource "local_file" "ingress_default" {
-  filename = "${local.workdir}/manifests/zz-02-ingress-default-internal.yaml"
-  content  = <<-YAML
-    apiVersion: operator.openshift.io/v1
-    kind: IngressController
-    metadata:
-      name: default
-      namespace: openshift-ingress-operator
-    spec:
-      domain: ${local.apps_domain}
-      endpointPublishingStrategy:
-        type: LoadBalancerService
-        loadBalancer:
-          scope: Internal
-          providerParameters:
-            type: AWS
-            aws:
-              type: NLB
-  YAML
+  filename = "${local.workdir}/manifests/zz-02-ingresscontroller.yaml"
+  content  = <<YAML
+apiVersion: operator.openshift.io/v1
+kind: IngressController
+metadata:
+  name: default
+  namespace: openshift-ingress-operator
+spec:
+  endpointPublishingStrategy:
+    type: LoadBalancerService
+    loadBalancer:
+      scope: Internal
+      providerParameters:
+        type: AWS
+        aws:
+          type: NLB
+YAML
 
   depends_on = [null_resource.ensure_manifests_dir]
 }
