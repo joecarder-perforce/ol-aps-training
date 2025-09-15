6ec28e8b2cc67e98660c123254d67acdaa653c31
Joe Carder <jcarder@perforce.com>
2025-09-15 06:19:43 +0200

    fixed dual cluster VPC associartion

--- dns-add.tf
+++ dns-add.tf
@@ -12,14 +12,6 @@ locals {
   }
 }
 
-## Current AWS region (used by zone association)
-data "aws_region" "current" {}
-
-## Discover the cluster VPC by tag (Cluster=<clusterKey>)
-data "aws_vpc" "cluster" {}
-
-
-
 resource "aws_route53_zone" "private" {
   for_each = local.active_clusters
 
@@ -41,13 +33,4 @@ resource "aws_route53_zone" "private" {
       "kubernetes.io/cluster/${coalesce(try(local.infra_id_by_cluster[each.key], null), each.key)}" = "owned"
     }
   )
-}
-
-## Ensure the PHZ is associated with the **cluster VPC** (not just the jump VPC)
-resource "aws_route53_zone_association" "cluster_vpc" {
-  for_each   = local.active_clusters
-  zone_id    = aws_route53_zone.private[each.key].zone_id
-  vpc_id     = module.cluster[each.key].vpc_id
-  vpc_region = data.aws_region.current.id
-  depends_on = [module.cluster]
 }
